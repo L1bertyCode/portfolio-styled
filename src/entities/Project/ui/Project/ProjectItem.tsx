47996b8895c965c2ec35e658335a1f3eb2b14c21
LC <libertycodeg@gmail.com>
2024-02-10 14:38:37 +0300

    ch: sh/ui/AL, e/P/PI

--- src/entities/Project/ui/Project/ProjectItem.tsx
+++ src/entities/Project/ui/Project/ProjectItem.tsx
@@ -2,7 +2,7 @@ import { memo } from "react";
 import styled from "styled-components";
 import { ProjectSchema } from "../../model/types/project";
 import { Text } from "../../../../shared/ui/Text/Text";
-import { Button } from "../../../../shared/ui/Button/Button";
+
 import { AppLink } from "../../../../shared/ui/AppLink/AppLink";
 
 interface ProjectItemProps {
@@ -19,18 +19,20 @@ export const ProjectItem = memo(
    return null;
   }
   return (
-   <StyledProjectItem className={`${className}`}>
+   <StyledProjectItem className={className}>
     <div className={"imgBlock"}>
-     {project.imgUrl ? (
+     {project.imgUrl && (
       <img
        className="img"
        src={project.imgUrl}
        alt={project.title}
       />
-     ) : (
-      <div />
      )}
-     <AppLink className="link" to={"/project" + project.id}>
+     <AppLink
+      variant="filled"
+      className={"link"}
+      to={"/project" + project.id}
+     >
       Show poject
      </AppLink>
     </div>
@@ -56,35 +58,51 @@ export const ProjectItem = memo(
  }
 );
 const StyledProjectItem = styled.div<ProjectItemProps>`
- width: 48%;
+ max-width: 48%;
  height: 638px;
  background: var(--card-color);
  border-radius: var(--b-rad-small);
  overflow: hidden;
+ transition-delay: var(--transition-delay);
 
  .imgBlock {
   width: 100%;
+  height: 388px;
+  border-radius: 12px 12px 0 0;
+  border: 1px solid var(--accent-color);
   position: relative;
+  transition-delay: var(--transition-delay);
+  background: var(--background-color);
+
   .img {
+   border-radius: 12px 12px 0 0;
    width: 100%;
    height: 388px;
-  }
-  div {
-   width: 100%;
-   height: 388px;
-   background: var(--accent-gradient);
-   .link {
-    position: absolute;
-    top: 50%;
-   }
   }
 
-  height: 388px;
-  background: var(--accent-gradient);
+  .link {
+   display: none;
+   position: absolute;
+   top: 50%;
+   left: 50%;
+   transform: translate3d(-50%, -50%, 0);
+   transition-delay: var(--transition-delay);
+  }
+ }
+ &:hover {
+  transition-delay: var(--transition-delay);
+  .imgBlock {
+   background: #00000001;
+   transition-delay: var(--transition-delay);
+   .link {
+    transition-delay: var(--transition-delay);
+    display: block;
+    transition-delay: var(--transition-delay);
+   }
+  }
  }
  .content {
   width: 100%;
-
   padding: 12px;
  }
  .title {
--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -4,67 +4,90 @@ import {
  LinkProps,
  useLocation,
 } from "react-router-dom";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+type AppLinkVariant = "clear" | "filled";
 interface AppLinkProps extends LinkProps {
  children: ReactNode;
  to: string;
+ variant?: AppLinkVariant;
  className?: string;
 }
-export const AppLink = (props: AppLinkProps) => {
+
+const DefaultAppLink = (props: AppLinkProps) => {
  const { children, to, className, ...otherProps } = props;
  const location = useLocation();
  const isActive = location.pathname === to;
 
  return (
-  <StyledAppLink
+  <Link
    className={`${className} ${isActive ? "isActive" : " "}`}
+   {...otherProps}
+   to={to}
   >
-   <Link {...otherProps} to={to}>
-    {children}
-   </Link>
-  </StyledAppLink>
+   {children}
+  </Link>
  );
 };
-const StyledAppLink = styled.p`
- a {
-  position: relative;
-  padding: 12px 6px;
-  text-decoration: none;
-  border-bottom: 11px solid transparent;
-  border-radius: var(--b-rad-small);
-  transition: var(--transition-delay);
-  color: var(--text-color);
-  &:after {
-   transition: var(--transition-delay);
-   position: absolute;
-   content: "";
-   width: 100%;
-   height: 11px;
-   background: transparent;
-   bottom: -4px;
-   left: 0;
-   border-radius: var(--b-rad-small);
+export const AppLink = styled(DefaultAppLink)`
+ text-decoration: none;
+ color: var(--text-color);
+ ${(props: AppLinkProps) => {
+  switch (props.variant) {
+   case "filled":
+    return css<AppLinkProps>`
+     padding: 12px 20px;
+     border-radius: var(--b-rad-small);
+
+     background: var(--accent-gradient);
+     color: var(--background-color);
+     &:hover {
+      border: 1px solid var(--accent-color);
+      background: none;
+      color: var(--accent-color);
+      transition: var(--transition-delay);
+     }
+    `;
+   default:
+    return css<AppLinkProps>`
+     position: relative;
+     padding: 12px 6px;
+     border-bottom: 11px solid transparent;
+     border-radius: var(--b-rad-small);
+     transition: var(--transition-delay);
+     &:after {
+      transition: var(--transition-delay);
+      position: absolute;
+      content: "";
+      width: 100%;
+      height: 11px;
+      background: transparent;
+      bottom: -4px;
+      left: 0;
+      border-radius: var(--b-rad-small);
+     }
+     &:hover {
+      transition: var(--transition-delay);
+      &:after {
+       transition: var(--transition-delay);
+       background: var(--accent-gradient);
+      }
+     }
+     &.isActive {
+      transition: var(--transition-delay);
+      &:after {
+       transition: var(--transition-delay);
+       position: absolute;
+       content: "";
+       width: 100%;
+       height: 11px;
+       background: var(--accent-gradient);
+       bottom: -4px;
+       left: 0;
+       border-radius: var(--b-rad-small);
+      }
+     }
+    `;
   }
-  &:hover {
-   transition: var(--transition-delay);
-   &:after {
-    transition: var(--transition-delay);
-    background: var(--accent-gradient);
-   }
-  }
-  &.isActive {
-   transition: var(--transition-delay);
-   &:after {
-    transition: var(--transition-delay);
-    position: absolute;
-    content: "";
-    width: 100%;
-    height: 11px;
-    background: var(--accent-gradient);
-    bottom: -4px;
-    left: 0;
-    border-radius: var(--b-rad-small);
-   }
-  }
- }
+ }}
 `;
